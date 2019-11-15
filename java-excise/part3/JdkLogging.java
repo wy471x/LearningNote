c6ae74a54f8cb2877c80abfd82e20c9934f64cf3
wy471x <52033069+wy471x@users.noreply.github.com>
2019-11-15 09:46:44 +0800

    Add files via upload

--- java-excise/part3/JdkLogging.java
+++ java-excise/part3/JdkLogging.java
@@ -1,16 +1,26 @@
 package ExceptionHandling;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import java.io.UnsupportedEncodingException;
 
 public class JdkLogging {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-        Logger logger = Logger.getGlobal();
-        logger.info("start process...");
-        logger.warning("memory is running out...");
-        logger.fine("ignored.");
-        logger.severe("process will be terminated...");
+//        Logger logger = Logger.getGlobal();
+//        logger.info("start process...");
+//        logger.warning("memory is running out...");
+//        logger.fine("ignored.");
+//        logger.severe("process will be terminated...");
+		//excise
+		Logger logger = Logger.getLogger(JdkLogging.class.getName());
+		logger.info("Start process...");
+		try {
+			"".getBytes("invalidCharsetName");
+		}catch(UnsupportedEncodingException e){
+			logger.severe("Exception");
+		}
+		logger.info("Process end.");
 	}
 
 }
